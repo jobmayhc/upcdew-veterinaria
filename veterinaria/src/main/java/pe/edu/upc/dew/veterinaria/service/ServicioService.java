84968bbf69bd42e74ef4c57ec628cf8b1493e7d1
carlo.qa <carlo.qa@372828f9-878f-6e63-0516-b92f7b0b3529>
2010-10-31 09:08:19 +0000

    Modify for CSalvatierra 31-10-2010 04:08 horas

---
+++ veterinaria/src/main/java/pe/edu/upc/dew/veterinaria/service/ServicioService.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pe.edu.upc.dew.veterinaria.service;
+
+/**
+ *
+ * @author Administrador
+ */
+
+import pe.edu.upc.dew.veterinaria.model.Servicio;
+
+public interface ServicioService {
+
+    public Boolean Registrar(String servicio,String precioCosto,String precioVenta, String estado);
+
+}
